70ff6da34a7ce48551cc8e7b424a41888faf0909
AntiVlad <vladnyash@gmail.com>
2022-09-24 01:32:48 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,25 +14,27 @@
 
                 <div class="card-body d-flex justify-content-between">
                     <div>
-                        <h2>Welcome To Your Dashboard {{ Auth::user()->name }}</h2> 
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                        <div>
+                            <h1>Welcome To Your Dashboard {{ Auth::user()->name }}</h1> 
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
 
-                        @endif
-                    </div>
-                    
-                    <div>
-                        <form action="/up" method='POST' enctype="multipart/form-data" class="float-right">
-                            @csrf
-                            <img style="width: 200px" src={{asset('/storage/images/'. Auth::user()->avatar)}} alt="bread" >
-                            <Label>Upload Profile image</Label><br>
-                            <input type="File" name='avatar'> <br>
-                            <input class="btn btn-primary" type="submit">
-                        </form>
-                    </div>
-                    <div class="ml-auto">
-                        <a href="/post" class="btn btn-secondary" >Create a Post</a>
+                            @endif
+                        </div>
+                        
+                        <div>
+                            <form action="/up" method='POST' enctype="multipart/form-data" class="float-right">
+                                @csrf
+                                <img style="width: 200px" src={{asset('/storage/images/'. Auth::user()->avatar)}} alt="bread" >
+                                <Label>Upload Profile image</Label><br>
+                                <input type="File" name='avatar'> <br>
+                                <input class="btn btn-primary" type="submit">
+                            </form>
+                        </div>
+                        <div class="ml-auto">
+                            <a href="/post" class="btn btn-secondary" >Create a Post</a>
+                        </div>
                     </div>
                 </div>
             </div>
