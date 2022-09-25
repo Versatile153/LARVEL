6e2e6c28503c88b95e71c3804fda43330ab050c8
Smartsaint <106387586+Smartsaint@users.noreply.github.com>
2022-09-24 21:32:15 -0700

    My create page

--- laravelBlog/database/migrations/2022_09_21_190922_posts.php
+++ laravelBlog/database/migrations/2022_09_21_190922_posts.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('posts', function (Blueprint $table){
+              $table->increments('id');
+              $table->longText('title');
+              $table->longText('description');
+              $table->string('image_path');
+              $table->timestamps();
+              
+            //   $table->foreign('user_id')->reference('id')->on('users');
+
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        //
     }
 };
