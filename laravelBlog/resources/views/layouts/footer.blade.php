a8d80934d1ea24ef7d7f071b0a8caff8cd4f5de8
Kellaben <108864110+Kellaben@users.noreply.github.com>
2022-09-28 10:06:55 +0100

    first commit

---
+++ laravelBlog/resources/views/layouts/footer.blade.php
@@ -0,0 +1,88 @@
+
+<link rel="stylesheet" href="{{ asset('css/blog.css') }}">
+<footer class="footer">
+
+    <div class="card-body bg-dark mb-3">
+        <div class="container text-center">
+            <div class="row">
+              <div class="col">
+                <h3>
+                    pages
+                </h3>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+        
+                    <li>
+                        <a href="/blog">Blog</a>
+                    </li>
+        
+                    <li>
+                        <a href="/login">Login</a>
+                    </li>
+                    <li>
+                        <a href="/register">Register</a>
+                    </li>
+                </ul>
+               
+              </div>
+    
+    
+              <div class="col">
+                <h3>
+                    Latest Posts
+                </h3>
+    
+                <ul>
+                    <li>
+                        <a href="/">Why we love tech</a>
+                    </li>
+        
+                    <li>
+                        <a href="/">why we love Blogging</a>
+                    </li>
+        
+                    <li>
+                        <a href="/">why we love tech</a>
+                    </li>
+                    <li>
+                        <a href="/">why laravel</a>
+                    </li>
+                </ul>
+              </div>
+    
+    
+              <div class="col">
+                <h3>
+                    Find Us
+                </h3>
+                
+                <ul>
+                    <li>
+                        <a href="/">What we Do</a>
+                    </li>
+        
+                    <li>
+                        <a href="/">Adress</a>
+                    </li>
+        
+                    <li>
+                        <a href="/">Phone</a>
+                    </li>
+                    <li>
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+    
+              </div>
+            </div>
+          </div>
+    
+            
+        <p class="c_right" style="text-align: center"> Copywrite&#169 DeveloperForum 2022, All Right Reserver</p>
+    
+</footer>
+    
+
+
--- laravelBlog/routes/web.php
+++ laravelBlog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homController;
+use App\Http\Controllers\pagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[pagesController::class, 'index']);
+
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
